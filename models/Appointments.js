93ccdd983d4e3fc3b8b5e1398525abd4e43f3b7b
Paul Burgess <paul@burgess.io>
2024-05-14 14:47:54 -0400

    Create Appointment model

---
+++ models/Appointments.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const appointmentSchema = new Schema({
+  barber: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Barber'
+    }],
+  date: Date,
+  time: Number,
+}, {timestamps: true})
+
+module.exports = mongoose.model("Barber", barberSchema)
